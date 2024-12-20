e8f21bca81fb2336cc0efed7b6e4f5c80f04b9a5
Omar Kasera <omarkasera50@gmail.com>
2024-12-20 18:40:16 +0300

    Update.

--- src/Components/Home/Social.jsx
+++ src/Components/Home/Social.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { RiTwitterXLine } from "react-icons/ri";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { FiGithub } from "react-icons/fi";
 
 const Social = () => {
   return (
     <div className="home__social">
       <a href="#" className="home__social-icon" target="_blank">
-        <i className="bx bxl-twitter"></i>
+        <RiTwitterXLine />
       </a>
 
       <a href="#" className="home__social-icon" target="_blank">
-        <i className="uil uil-linkedin-alt"></i>
+        <SlSocialLinkedin />
       </a>
 
       <a href="#" className="home__social-icon" target="_blank">
-        <i className="uil uil-github-alt"></i>
+        <FiGithub />
       </a>
     </div>
   );
